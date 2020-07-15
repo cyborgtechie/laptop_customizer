4a6ed7d26cc75fa52c38d817bd8dc56eda0c56c7
Maria Guerrero <guerrermj@gmail.com>
2020-07-15 11:54:01 -0400

    updated App.js to fix updateFeature bug

--- src/App.js
+++ src/App.js
@@ -11,26 +11,30 @@ import Cart from "./Cart/Cart";
 // easily convert numbers into US dollar values
 
 class App extends Component {
-  state = {
-    selected: {
-      Processor: {
-        name: "17th Generation Intel Core HB (7 Core with donut spare)",
-        cost: 700
-      },
-      "Operating System": {
-        name: "Ubuntu Linux 16.04",
-        cost: 200
-      },
-      "Video Card": {
-        name: "Toyota Corolla 1.5v",
-        cost: 1150.98
-      },
-      Display: {
-        name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
-        cost: 1500
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selected: {
+        Processor: {
+          name: "17th Generation Intel Core HB (7 Core with donut spare)",
+          cost: 700
+        },
+        "Operating System": {
+          name: "Ubuntu Linux 16.04",
+          cost: 200
+        },
+        "Video Card": {
+          name: "Toyota Corolla 1.5v",
+          cost: 1150.98
+        },
+        Display: {
+          name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+          cost: 1500
+        }
       }
-    }
-  };
+    };
+  }
 
   updateFeature = (feature, newValue) => {
     const selected = Object.assign({}, this.state.selected);
@@ -50,9 +54,9 @@ class App extends Component {
           <Customize
             selected={this.state.selected}
             features={this.props.features}
+            updateFeature={this.updateFeature}
           />
           <Cart selected={this.state.selected} />
-          
         </main>
       </div>
     );
